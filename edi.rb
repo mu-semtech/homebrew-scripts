7584b09c19c3ed252c1406549f2020c3f476e4c5
Aad Versteden <madnificent@gmail.com>
2017-11-13 16:48:04 +0100

    Add url with revision to Formula for versioned install
    We are trying to understand brew based on example we discover online.

--- edi.rb
+++ edi.rb
@@ -2,8 +2,11 @@ require 'formula'
 
 class Edi < Formula
   homepage 'http://mu.semte.ch'
-  head 'https://github.com/madnificent/docker-ember.git'
+  url 'https://github.com/madnificent/docker-ember.git',
+      :revision => "03f59a31f6cc172a4c8c8a8e68c39569fdc70d40"
   version '2.14.0'
+
+  head 'https://github.com/madnificent/docker-ember.git'
 
   def install
     bin.install 'bin/ed', 'bin/edi', 'bin/edl', 'bin/eds'
