06a4300ac2bb994a2073ca867b67817dd8a94cc6
Aad Versteden <madnificent@gmail.com>
2017-11-13 16:34:44 +0100

    Add version to edi formula
    The formula for edi did not have a version.  We add this so we can push updates to edi as needed.  As edi itself follows the ember version numbers sparsely, this formula will do the same.

--- edi.rb
+++ edi.rb
@@ -3,6 +3,7 @@ require 'formula'
 class Edi < Formula
   homepage 'http://mu.semte.ch'
   head 'https://github.com/madnificent/docker-ember.git'
+  version '2.14.0'
 
   def install
     bin.install 'bin/ed', 'bin/edi', 'bin/edl', 'bin/eds'
