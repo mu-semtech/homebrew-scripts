66dee921ff907abe7ba430b41b84ce47e118a693
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2018-01-05 15:59:02 +0100

    updated versions

--- edi.rb
+++ edi.rb
@@ -4,7 +4,7 @@ class Edi < Formula
   homepage 'http://mu.semte.ch'
   url 'https://github.com/madnificent/docker-ember.git',
       :revision => "4f10c3169f8ac66542f39788180f55621a7541cb"
-  version '2.17.0'
+  version '2.17.1'
 
   head 'https://github.com/madnificent/docker-ember.git'
 
