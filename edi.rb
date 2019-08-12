18f40f42cb8c67af3757988c237a8e2833a57f25
Aad Versteden <madnificent@gmail.com>
2019-08-12 13:02:26 +0200

    Updating versions

--- edi.rb
+++ edi.rb
@@ -3,8 +3,8 @@ require 'formula'
 class Edi < Formula
   homepage 'http://mu.semte.ch'
   url 'https://github.com/madnificent/docker-ember.git',
-      :revision => "94a730e16aff7965b6974e57d169fdc63b0be921"
-  version '3.0.1'
+      :revision => "31f1b5bfd51e15ee7a66fb27c9e80e17a37cec38"
+  version '3.10.1'
 
   head 'https://github.com/madnificent/docker-ember.git'
 
